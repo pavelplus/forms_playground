6ea544e5e7b3a3684fbb4a846c4794f1babd8852
pavelplus <pavel.irf@gmail.com>
2025-12-19 17:43:16 +0300

    all basic elements done

--- forms_pg/main/forms.py
+++ forms_pg/main/forms.py
@@ -9,11 +9,19 @@ class TestForm(forms.Form):
     ]
     CONTACT_TYPE_CHIOCES = [
         (None, 'не беспокоить'),
-        ('email', 'мужской'),
-        ('phone', 'женский'),
-        ('whatsapp', 'женский'),
-        ('telegram', 'женский'),
+        ('email', 'E-Mail'),
+        ('phone', 'Телефон'),
+        ('whatsapp', 'Whatsapp'),
+        ('telegram', 'Telegram'),
     ]
+    COLORS_CHOICES = [
+        ('R', 'Red'),
+        ('G', 'Green'),
+        ('B', 'Blue'),
+        ('W', 'White'),
+        ('B', 'Black'),
+    ]
+
     USERNAME_MAX_LENGTH = 10
 
     prefix="testform"
@@ -29,7 +37,8 @@ class TestForm(forms.Form):
     password2 = forms.CharField(min_length=8, label='Пароль (повторно)', help_text='Повторите введенный пароль', widget=forms.PasswordInput())
     age = forms.IntegerField(label='Возраст', help_text='Для проверки на совершеннолетие')
     sex = forms.ChoiceField(choices=SEX_CHOICES, label='Пол', help_text='Для уточнения предлагаемых сервисов', required=False, widget=forms.RadioSelect())
+    colors = forms.MultipleChoiceField(choices=COLORS_CHOICES, label='Любимые цвета', help_text='Можно выбрать несколько')
     contact_type = forms.ChoiceField(choices=CONTACT_TYPE_CHIOCES, label='Способ связи', help_text='Как направлять вам важную информацию?', required=False)
-    about = forms.CharField(label="О себе", help_text="Поделитесь информацией с другими пользователями", required=False, widget=forms.Textarea())
+    about = forms.CharField(label="О себе", help_text="Поделитесь информацией с другими пользователями", required=False, widget=forms.Textarea(attrs={'rows':'3'}))
     autologin = forms.BooleanField(label='Запомнить', help_text='Входить автоматически')
     token = forms.CharField(widget=forms.HiddenInput())
